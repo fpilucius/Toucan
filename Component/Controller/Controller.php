f7c5e94d88c1b9a81992dd2cc5cd7401a9ba1f6d
fpilucius <fpilucius@gmail.com>
2017-01-10 11:42:45 +0100

    documentation

--- Component/Controller/Controller.php
+++ Component/Controller/Controller.php
@@ -1,4 +1,10 @@
 <?php
+
+/**
+ *
+ *
+ */
+
 namespace Toucan\Component\Controller;
 
 use Toucan\Component\Registre\Registry;
@@ -6,31 +12,27 @@ use Toucan\Component\Dependency\Container;
 use Toucan\Core\Http\Response;
 use Toucan\Core\Http\ResponseRedirect;
 
-class Controller
-{
+class Controller {
+
     protected $container;
-    
-    public function __construct()
-    {
+
+    public function __construct() {
         $this->container = Registry::get('container');
     }
-    
-    public function render($view, array $parameters = null)
-    {
+
+    public function render($view, array $parameters = null) {
         $rendered = $this->get('template')->render($view, $parameters, null);
         return new Response($rendered);
-        
     }
-    
-    public function redirect($url, $status = 302)
-    {
+
+    public function redirect($url, $status = 302) {
         return new ResponseRedirect($url, $status);
     }
-    
-    public function get($key)
-    {
+
+    public function get($key) {
         return $this->container->getService($key);
     }
-    
+
 }
+
 ?>
